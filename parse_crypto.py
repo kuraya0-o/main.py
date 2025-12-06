30232d831927cdec8790d53d07f3ce93f108170b
kuraya0-o <lloyd1702@gmail.com>
2025-12-06 04:52:00 +0300

    start

---
+++ parse_crypto.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup as Bs
+
+def main():
+    count = input("Введите количество записей или оставьте поле пустым ->")
+    if count.isdigit():
+        
+
+if __name__ == "__main__":
+    main()
