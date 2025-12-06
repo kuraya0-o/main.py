c13fe53d84775020fa13599208efeaace0547ce7
kuraya0-o <lloyd1702@gmail.com>
2025-12-06 05:13:18 +0300

    parsing

--- parse_crypto.py
+++ parse_crypto.py
@@ -7,6 +7,21 @@ def printArr(arr: list, count: int):
     for i in range(min(len(arr), count)):
         print(f"{arr[i][0]:>60} {arr[i][1]:^70} {arr[i][2]:>10.2f}")
 
+def parseHtml(html: str) -> list:
+    arr = []
+    soup = Bs(html, "html.parser")
+    table = soup.find("table")
+    trs = table.find_all("tr")
+    for tr in trs[1:]:
+        tds = tr.find_all("td")
+        name = tds[1].find_all("div")[1].text.strip().replace("\n", "")
+        href = "https://etherscan.io" + tds[1].find("a").get("href").strip()
+        priceMain = tds[3].find("div").text.strip()
+        price = float(priceMain.replace("$", "").replace(",", "").replace(" ", ""))
+        arr.append([name, href, price])
+
+    return arr
+
 
 
 def getHtml() -> str:
